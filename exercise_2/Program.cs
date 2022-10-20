627141081a913df47100a514743a83e74b2b5057
Ruslan <mephistopooh45@gmail.com>
2022-10-20 22:00:21 +0500

    exercise_2

--- exercise_2/Program.cs
+++ exercise_2/Program.cs
@@ -1,10 +1,25 @@
-﻿Console.WriteLine($"Введите 1-е число");
-double a = Convert.ToDouble(Console.ReadLine());
+﻿string checknum()
+{
+    while(true)
+    {
+        string text = Console.ReadLine();
+        if (double.TryParse(text, out double number))         // функция проверки ввода пользователем
+        {
+            return text;
+            break;
+        }
+Console.WriteLine("Некорректный ввод, введите ЧИСЛО!");
+}
+   
+}
+
+Console.WriteLine($"Введите 1-е число");
+double a = Convert.ToDouble(checknum());
 double max = a;
 for (int i = 0; i < 2; i++)
 {
     Console.WriteLine($"Введите {i+2}-е число");
-    double b = Convert.ToDouble(Console.ReadLine());
+    double b = Convert.ToDouble(checknum());
     if (b > max)
     {
         max = b;
