2bf1bf6853135323bdc83563be4767357f7a4ca9
Ruslan <mephistopooh45@gmail.com>
2022-10-20 21:19:29 +0500

    exercise_2

---
+++ exercise_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Введите 1-е число");
+double a = Convert.ToDouble(Console.ReadLine());
+double max = a;
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Введите {i+2}-е число");
+    double b = Convert.ToDouble(Console.ReadLine());
+    if (b > max)
+    {
+        max = b;
+    }
+}
+Console.WriteLine($"Максимальное число = {max}");
