a375145b32dc791ed04ca271fc3325d6c53795a1
Ruslan <mephistopooh45@gmail.com>
2022-10-20 22:07:58 +0500

    exercise_3

---
+++ exercise_3/Program.cs
@@ -0,0 +1,23 @@
+﻿string checknum()
+{
+    while(true)
+    {
+        string text = Console.ReadLine();
+        if (double.TryParse(text, out double number))         // функция проверки ввода пользователем
+        {
+            return text;
+            break;
+        }
+Console.WriteLine("Некорректный ввод, введите ЧИСЛО!");
+}
+}
+Console.WriteLine("Введите число: ");
+double a = Convert.ToDouble(checknum());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} является четным");
+}
+else
+{
+    Console.WriteLine($"Число {a} не является четным");
+}
