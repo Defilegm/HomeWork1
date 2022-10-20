b6d68882d6cd3f607a04e4cb4332e377505e2b95
Ruslan <mephistopooh45@gmail.com>
2022-10-20 22:36:19 +0500

    exercise_4

---
+++ exercise_4/Program.cs
@@ -0,0 +1,35 @@
+﻿string checknum()
+{
+    while(true)
+    {
+        string text = Console.ReadLine();
+        if (double.TryParse(text, out double number))         // функция проверки ввода пользователем
+        {
+            return text;
+            break;
+        }
+Console.WriteLine("Некорректный ввод, введите ЧИСЛО!");
+}  
+}
+Console.WriteLine("Введите число: ");
+double a = Convert.ToDouble(checknum());
+while(a != 0 )
+{
+    if(a > 0)
+    {
+        if(a % 2 == 0)
+        {
+            Console.Write($" {a}");
+        }
+        a = a - 1;
+        
+    }
+    else if(a < 0)
+    {
+        if(a % 2 == 0)
+        {
+            Console.Write($" {a}");
+        }
+        a++;
+    }
+}
