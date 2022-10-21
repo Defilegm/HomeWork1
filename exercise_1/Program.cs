12ea15518ef6bdd1a32692c55adccff10831ab32
Ruslan <mephistopooh45@gmail.com>
2022-10-22 02:56:10 +0500

    exercise_1

---
+++ exercise_1/Program.cs
@@ -0,0 +1,23 @@
+﻿int check()
+{
+    while(true)
+    {
+
+        Console.WriteLine("Введите трехзначное число: ");
+        string x = Console.ReadLine();
+        if(int.TryParse(x,out int a))
+        {   
+            if (a >= 100 && a < 1000)
+            {
+                return a;
+            }
+        }
+        Console.WriteLine("Некорректный ввод!");
+    }
+    
+
+}
+
+
+int b = check(); 
+Console.WriteLine($"Вторая цифра: {b / 10 % 10}");
