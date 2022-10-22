efdea310ef6860bc43020621d5496dad7ba4c475
Ruslan <mephistopooh45@gmail.com>
2022-10-22 17:34:52 +0500

    exercise_2

---
+++ exercise_2/Program.cs
@@ -0,0 +1,6 @@
+﻿Random rnd = new Random();
+int number = rnd.Next(100,1000);
+Console.WriteLine($"Случайное число: {number}");
+int newnumber = number / 100;
+int newnumber2 = number % 10;
+Console.WriteLine($"Удаление второй цифры...: {newnumber}"+newnumber2) ;
