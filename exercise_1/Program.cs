be379c1e9d6b7896c142682ebd23b114cfcd8eaa
Ruslan <mephistopooh45@gmail.com>
2022-10-23 03:31:18 +0500

    exercise_1

--- exercise_1/Program.cs
+++ exercise_1/Program.cs
@@ -3,11 +3,11 @@
     while(true)
     {
 
-        Console.WriteLine("Введите трехзначное число: ");
+        Console.WriteLine("Введите пятизначное число: ");
         string x = Console.ReadLine();
         if(int.TryParse(x,out int a))
         {   
-            if (a >= 100 && a < 1000)
+            if (a >= 10000 && a < 100000)
             {
                 return a;
             }
@@ -15,5 +15,13 @@
         Console.WriteLine("Некорректный ввод!");
     }
 }
-int b = check(); 
-Console.WriteLine($"Вторая цифра: {b / 10 % 10}");
+int b = check();
+if (b % 10 == b / 10000 && (b % 100) / 10 == (b / 1000) % 10)
+{
+    Console.WriteLine($"Число:{b} является палиндромом");
+
+}
+else
+{
+    Console.WriteLine($"Число:{b} не является палиндромом");
+}
