9d3293bad75d8e122ea37afb2081e7fa6a6e1834
Ruslan <mephistopooh45@gmail.com>
2022-10-22 20:21:30 +0500

    exercise_4

---
+++ exercise_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int checknum()
+{
+    while(true)
+    {
+        Console.WriteLine("Введите цифру от 1 до 7: ");
+        string number = Console.ReadLine();
+        if (int.TryParse(number, out int numbint))
+        {
+            if(numbint < 8 && numbint > 0)
+            {
+                return numbint;
+            }
+        }
+    Console.WriteLine("Некорректный ввод!");
+    } 
+}
+int dayoff = checknum();
+if (dayoff == 6 || dayoff == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Невыходной день");
+}
