da3df141f26540483c9f44cd797f67068ffc6743
Ruslan <mephistopooh45@gmail.com>
2022-10-22 19:20:29 +0500

    exercise_3

---
+++ exercise_3/Program.cs
@@ -0,0 +1,37 @@
+﻿int check()
+{
+    while(true)
+    {
+        Console.WriteLine("Введите число: ");
+        string number = Console.ReadLine();
+        if(int.TryParse(number, out int numberint))
+        {
+            return numberint;
+            
+        }
+    Console.WriteLine("Некорректный ввод!");
+    }
+}
+int number = check();  
+
+int numberdouble = 0;
+if (number / 100 > 0)
+{
+    while (number / 100 > 0)
+    {
+    numberdouble = number;
+    number = number / 10;
+    
+
+    }
+Console.WriteLine($"Третья цифра заданного числа: {numberdouble%10}");
+}
+else
+{
+    Console.WriteLine("В заданном числе нет третьей цифры!");
+}
+
+
+
+
+
