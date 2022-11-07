aa9e799ae086e608a5f007cffd61afc4cf8ca3b7
Ruslan <mephistopooh45@gmail.com>
2022-11-08 04:38:45 +0500

    exercise_4

---
+++ exercise_4/Program.cs
@@ -0,0 +1,29 @@
+﻿int check(string x)
+{
+    while(true)
+    { 
+        if (int.TryParse(x, out int result))
+        {
+        return result;
+        }
+        Console.WriteLine("Введите целое число!");
+        x = Console.ReadLine();
+    }
+    
+}
+
+void expo()
+{
+    Console.WriteLine("Введите первое число");
+    int a = check(Console.ReadLine());
+    Console.WriteLine("Введите второе число число");
+    int b = check(Console.ReadLine());
+    int result = a;
+    while (b > 1)
+    {
+        result =  result * a;
+        b = b - 1;
+    }
+    Console.WriteLine(result);
+}
+expo();
