4a9c43ddb57ef28078a482764eb160eb414fe2ed
Ruslan <mephistopooh45@gmail.com>
2022-11-11 03:29:28 +0500

    exercise_3

---
+++ exercise_3/Program.cs
@@ -0,0 +1,26 @@
+﻿double[] array()
+{
+    Random rnd = new Random();
+    double [] array = new double [rnd.Next(1,10)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 101);
+    }
+    return array;
+}
+double[] array1 = array();
+
+double diff()
+{
+    double max = array1[0];
+    double min = array1[0];
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i] > max){max = array1[i];}
+        if (array1[i] < min){min = array1[i];}
+    }
+    double result = max - min;
+    return result;
+}
+Console.WriteLine(string.Join(" ", array1));
+Console.Write($"Разница между max и min: {diff()}");
