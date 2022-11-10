d7eac5e362c00aee6b2f92498ed0eb7daa921d0a
Ruslan <mephistopooh45@gmail.com>
2022-11-11 03:13:17 +0500

    exercise_2

---
+++ exercise_2/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] array()
+{
+    Random rnd = new Random();
+    int [] array = new int [rnd.Next(1,10)];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-100, 101);
+    }
+    return array;
+}
+int[] array1 = array();
+
+int checkpos()
+{
+    int summ  = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (i % 2 != 0){summ = summ + array1[i];}
+    }
+    return summ;
+}
+Console.WriteLine(string.Join(" ", array1));
+Console.Write($"Сумма элементов, стоящие на нечетных позициях: {checkpos()}");
