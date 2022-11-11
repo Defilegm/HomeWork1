f8f1d047601a2331e72fb5071d1bf0ae24f635f0
Ruslan <mephistopooh45@gmail.com>
2022-11-11 15:59:44 +0500

    exercise_4

---
+++ exercise_4/Program.cs
@@ -0,0 +1,41 @@
+﻿int check(string x)
+{
+    while (true)
+    {
+        if (int.TryParse(x, out int result)) { return result; }
+        Console.WriteLine("Введите целое число!");
+        x = Console.ReadLine();
+    }
+}
+
+int input = check(Console.ReadLine());
+
+int[] array()
+{
+    int[] array = new int[input];
+    return array;
+}
+
+int[] array1 = array();
+
+int requrse(int x = 1, int i = 0, int count = 0)
+{
+    if(i < input)
+    {
+        while (count < x && i < input)
+        {
+            array1[i] = x;
+            i++;
+            count++;
+        }   
+        count = 0;
+        x++;
+        return requrse(x, i, count = 0);
+    }
+    return x;
+    
+}
+
+requrse();
+Console.WriteLine(string.Join(" ", array1));
+
